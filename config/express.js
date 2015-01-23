7ed0bf254fbcaa2b179735e881f939ea48ba6dfb
Martin Choraine <mchoraine@deolan.com>
2015-01-23 08:48:45 +0100

    Add Swagger UI

--- config/express.js
+++ config/express.js
@@ -11,63 +11,57 @@ var methodOverride = require('method-override');
 var swagger = require('swagger-express');
 
 module.exports = function(app, config) {
-  app.set('views', config.root + '/app/views');
-  app.set('view engine', 'ejs');
+    app.set('views', config.root + '/app/views');
+    app.set('view engine', 'ejs');
 
-  // app.use(favicon(config.root + '/public/img/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
-  app.use(compress());
-  app.use(express.static(config.root + '/public'));
-  app.use(methodOverride());
+    // app.use(favicon(config.root + '/public/img/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(express.static(config.root + '/public'));
+    app.use(methodOverride());
 
+    app.use(swagger.init(app, {
+        apiVersion: '1.0',
+        swaggerVersion: '1.0',
+        basePath: 'http://localhost:3000',
+        swaggerUI: './public/swagger/',
+        apis: ['./api.yml']
+    }))
 
-  //swagger(app, {
-  //  title : 'express validation swagger',
-  //  statics : '/public/swagger/',
-  //  resources : '/swagger/',
-  //  applicationUrl : 'http://localhost:3000',
-  //  routes : [
-  //    { page : 'user', method : 'GET',    path: '/user', validation : validation.user.get },
-  //    { page : 'user', method : 'POST',   path: '/user', validation : validation.user.post },
-  //    { page : 'user', method : 'DELETE', path: '/user', validation : validation.user.del },
-  //    { page : 'user', method : 'PUT',    path: '/user', validation : validation.user.put }
-  //  ]
-  //});
+    var controllers = glob.sync(config.root + '/app/controllers/*.js');
+    controllers.forEach(function (controller) {
+        require(controller)(app);
+    });
 
-  var controllers = glob.sync(config.root + '/app/controllers/*.js');
-  controllers.forEach(function (controller) {
-    require(controller)(app);
-  });
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-  app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+    if(app.get('env') === 'development'){
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err,
+                title: 'error'
+            });
+        });
+    }
 
-  if(app.get('env') === 'development'){
     app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err,
-        title: 'error'
-      });
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {},
+            title: 'error'
+        });
     });
-  }
-
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {},
-      title: 'error'
-    });
-  });
 
 };
